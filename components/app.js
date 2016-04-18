dc2b5b7ed8e367c69f0c6641305316fab5fcd6ab
darioflorez <florez.wilmar@gmal.com>
2016-04-18 10:43:56 +0200

    components dir created

---
+++ components/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import React, {
+  PropTypes,
+  View,
+  Text,
+} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Hello Weather
+        </Text>
+      </View>
+    );
+  }
+}
