2c79aab519db501835beb0ccbde33d6e6ad7efda
Johan Lundgren <johanlundgren@Johans-MacBook-Air.local>
2016-04-20 17:14:53 +0200

    added style to profile

--- components/profile.js
+++ components/profile.js
@@ -6,8 +6,15 @@ import React, {
   View,
   TextInput,
   TouchableOpacity,
-  Text
+  Dimensions,
+  Text,
 } from 'react-native';
+
+import LinearGradient from 'react-native-linear-gradient';
+
+const {height, width} = Dimensions.get('window');
+
+var Icon = require('react-native-vector-icons/Ionicons')
 
 export default class Profile extends Component {
   constructor(props){
@@ -15,15 +22,18 @@ export default class Profile extends Component {
   };
   render() {
     return (
-      <View style={{flex:1, backgroundColor: 'black', opacity:0.8, justifyContent: 'center', alignItems: 'center'}}>
-        <View>
+      <View style={{flex:1,}}>
+        <LinearGradient colors={['#F7F7F7', 'black']} style={{flex:0.8, opacity:0.9}} />
+        <View style={{flex:0.2, width:width, backgroundColor:'#F7F7F7',justifyContent: 'flex-start', alignItems: 'center'}}>
           <TouchableOpacity onPress={this.props.onClose}>
-          <Text style={{color: 'white', padding: 10,fontSize: 20, borderWidth: 1, borderColor:'white', borderRadius:10}}> Tillbaka </Text>
+            <Icon style={{paddingRight:10}}name="ios-arrow-down" size={50} color="black"/>
           </TouchableOpacity>
-          <TouchableOpacity onPress={this.props.onLogout()}>
-          <Text style={{color: 'white', marginTop: 20,padding: 10,fontSize: 20, borderWidth: 1, borderColor:'white', borderRadius:10}}> Logga ut </Text>
+          <TouchableOpacity style={{flexDirection: 'row', padding:10, borderRadius:10, backgroundColor: 'white'}}onPress={this.props.onLogout()}>
+            <Icon style={{paddingRight:10}}name="ios-arrow-left" size={30} color="#007AFF"/>
+            <Text style={{color: 'black',padding: 2,fontSize: 20 }}> Logga ut </Text>
           </TouchableOpacity>
         </View>
+        
       </View>
     );
   }
