71f4063492c58450ef952e22c2495dff63af9788
Johan Lundgren <johan.emil.lundgren@gmail.com>
2016-04-29 15:59:39 +0200

    added contacts

--- components/contacts.js
+++ components/contacts.js
@@ -3,22 +3,55 @@
 import React, {
   PropTypes,
   View,
+  ListView,
   Text,
   StyleSheet,
-  TextInput,
-  Image,
   TouchableOpacity,
 } from 'react-native';
+
+// you can link this with:
+// rnpm link 'react-native-contacts'
+import Contacts from 'react-native-contacts';
 
 import { styles } from '../styles/loginForm'
 
 var Icon = require('react-native-vector-icons/FontAwesome')
 
 export default class Contact extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      contacts: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+    }
+  }
+  componentDidMount(){
+    Contacts.getAll((err, contacts) => {
+      if(err && err.type === 'permissionDenied'){
+        console.log("Error trying to get contacts", err)
+      } else {
+        console.log(contacts)
+        this.setState({
+          contacts: this.state.contacts.cloneWithRows(contacts)
+        })
+        console.log(this.state.contacts)
+      }
+    })
+  }
+
+  _renderRow(rowData, rowSeg, rowId){
+    return(
+      <View>
+       <Text>{rowData.givenName}</Text>
+      </View>
+    );
+  }
+
   render() {
     return (
-      <View style={{flex: 1, backgroundColor: 'orange'}}>
-      </View>
+      <ListView
+        dataSource={this.state.contacts}
+        renderRow={this._renderRow}
+      />
     );
   }
 }
