9bcd37d0703717acebb946f6ab2ab322726bf4da
darioflorez <florez.wilmar@gmal.com>
2016-05-06 17:04:39 +0200

    fsdk installed on android

--- android/app/src/main/java/com/react_weather/MainActivity.java
+++ android/app/src/main/java/com/react_weather/MainActivity.java
@@ -10,10 +10,20 @@ import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
+//fsdk
+import android.content.Intent;     // <--- import
+import android.os.Bundle;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
+// you have not added AppEventsLogger. if you want to do it, look at the fsdk docs
 public class MainActivity extends ReactActivity {
+     CallbackManager mCallbackManager;
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -39,13 +49,28 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected List<ReactPackage> getPackages() {
+      mCallbackManager = new CallbackManager.Factory().create();
+
         return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
             new RCTCameraPackage(),
             new ReactNativeContacts(),
             new LinearGradientPackage(),
             new AirPackage(),
             new VectorIconsPackage()
         );
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+      super.onActivityResult(requestCode, resultCode, data);
+      mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
